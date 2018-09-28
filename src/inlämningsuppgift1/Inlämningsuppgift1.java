06fa1c19a72c5c016586aaa51c54dad19785faf7
Farouk Aouni <aouni.farouk@gmail.com>
2018-09-28 10:30:33 +0200

    första commit

---
+++ src/inlämningsuppgift1/Inlämningsuppgift1.java
@@ -0,0 +1,15 @@
+/*
+ * Java Utveckling 18
+ */
+
+package inlämningsuppgift1;
+
+
+public class Inlämningsuppgift1 {
+
+   
+    public static void main(String[] args) {
+        System.out.println("Jag är första github kod");
+    }
+
+}
